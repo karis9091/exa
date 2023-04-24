91b64560d953491524f35c28ed2ecbcd11b38013
Frodriguez09 <37087731+Frodriguez09@users.noreply.github.com>
2023-04-23 17:20:39 -0700

    Se quitaron config de firebase

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,7 @@ import { getMessaging } from 'firebase/messaging';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCKr9ix19zusJE-OUYMzGMBHf4oDtfUIqA",
-    authDomain: "climapwa-61202.firebaseapp.com",
-    projectId: "climapwa-61202",
-    storageBucket: "climapwa-61202.appspot.com",
-    messagingSenderId: "452884069417",
-    appId: "1:452884069417:web:7bc1621f0907de9fb53038"
+    
 };
 
 // Initialize Firebase
